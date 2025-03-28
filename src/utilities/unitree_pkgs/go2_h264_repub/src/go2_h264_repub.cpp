a63c74aec026205e92cb3cfe9ef78c279ba1dec6
youwei.cao <gofar@outlook.com>
2025-03-28 16:07:35 +0800

    fix video pub

--- src/utilities/unitree_pkgs/go2_h264_repub/src/go2_h264_repub.cpp
+++ src/utilities/unitree_pkgs/go2_h264_repub/src/go2_h264_repub.cpp
@@ -8,7 +8,7 @@ public:
     H264Repub() : Node("h264_repub") {
         image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/camera/image/raw", 10);
 
-        std::string multicast_iface = "etho0";
+        std::string multicast_iface = "enp0s31f6";
         this->declare_parameter("multicast_iface", multicast_iface);
         this->get_parameter("multicast_iface", multicast_iface);
 
